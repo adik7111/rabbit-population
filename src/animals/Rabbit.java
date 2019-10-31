cd75c2829f2e183ce36f9c47197acfd389b334ec
K1631110 <adrian.kucharski@kcl.ac.uk>
2019-10-31 17:00:53 +0000

    Add constructor to Rabbit class

--- src/animals/Rabbit.java
+++ src/animals/Rabbit.java
@@ -1,4 +1,14 @@
 package animals;
 
 public class Rabbit {
+
+    private int id;
+    private int age;
+    private double hunger;      //between 0.00 and 1.00
+    private boolean gender;     //true = female false = male
+
+    public Rabbit(int id, int age, double hunger, boolean gender) {
+        this.id = id; this.age = age;
+        this.hunger = hunger; this.gender = gender;
+    }
 }
